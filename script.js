6d9d63ccb89ca04e3bdf6ed4806bdd88ca69807c
yashsoni0200 <yash9924408@gmail.com>
2026-06-15 15:16:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function showMessage() {
+
+    document.getElementById("message").innerHTML =
+    "Stay safe: Never click unknown links and always verify the sender before sharing information.";
+
+}
